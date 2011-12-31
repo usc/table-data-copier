a3dbcd5552fedb9d466779699e2c4a8681d6a388
lishunli.me@gmail.com <lishunli.me@gmail.com@fa5e0b39-550b-3b35-3821-e6856afb3cb5>
2011-12-31 07:20:19 +0000

    DEV: init import.
    git-svn-id: https://table-data-copier.googlecode.com/svn/trunk@2 fa5e0b39-550b-3b35-3821-e6856afb3cb5

---
+++ src/main/java/com/googlecode/usc/Utils.java
@@ -0,0 +1,109 @@
+package com.googlecode.usc;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ *
+ * @author ShunLi
+ */
+public class Utils {
+    public static boolean isBlank(CharSequence cs) {
+        int strLen;
+        if (cs == null || (strLen = cs.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if ((Character.isWhitespace(cs.charAt(i)) == false)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String buildInsertSql(Set<String> columnNames, String criteria) {
+        int indexOfWhere = criteria.toLowerCase().indexOf("where");
+
+        StringBuffer insertSql = new StringBuffer("INSERT INTO " + (indexOfWhere != -1 ? criteria.substring(0, indexOfWhere) : criteria) + "(");
+        insertSql.append(buildParams(columnNames, ""));
+        insertSql.append(") VALUES (");
+        insertSql.append(buildParams(columnNames, ":"));
+        insertSql.append(") ");
+        return insertSql.toString();
+    }
+
+    public static Properties loadPropertiesFile(String outSideConfigFile, InputStream inSideConfigFile) {
+        Properties prop = new Properties();
+
+        InputStream propertiesFile = null;
+        try {
+            File file = new File(outSideConfigFile);
+            if (file.exists()) {
+                propertiesFile = new FileInputStream(file);
+            } else {
+                propertiesFile = inSideConfigFile;
+            }
+
+            if (propertiesFile != null) {
+                prop.load(propertiesFile);
+            }
+        } catch (IOException e) {
+        }
+
+        return prop;
+    }
+
+    public static Properties loadPropertiesFile(InputStream propertiesFileName) {
+        Properties prop = new Properties();
+
+        InputStream propertiesFile = propertiesFileName;
+        try {
+            if (propertiesFile != null) {
+                prop.load(propertiesFile);
+            }
+        } catch (IOException e) {
+        }
+
+        return prop;
+    }
+
+    public static Boolean parseString2Boolean(String value, Boolean defaultValue) {
+        if ("true".equalsIgnoreCase(value)) {
+            return Boolean.TRUE;
+        } else if ("false".equalsIgnoreCase(value)) {
+            return Boolean.FALSE;
+        }
+
+        return defaultValue;
+    }
+
+    public static Long parseString2Long(String value, Long defaultValue) {
+        try {
+            return Long.parseLong(value);
+        } catch (NumberFormatException e) {
+        }
+
+        return defaultValue;
+    }
+
+    private static String buildParams(Set<String> columnNames, String prefix) {
+        StringBuffer params = new StringBuffer();
+
+        boolean isFirstColumn = true;
+        for (String columnName : columnNames) {
+            if (!isFirstColumn) {
+                params.append(", " + prefix + columnName);
+            } else {
+                params.append(prefix + columnName);
+                isFirstColumn = false;
+            }
+        }
+
+        return params.toString();
+    }
+
+}
---
+++ src/test/java/com/googlecode/usc/UtilsTest.java
@@ -0,0 +1,130 @@
+package com.googlecode.usc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author ShunLi
+ */
+public class UtilsTest {
+
+    @Test
+    public void testIsBlank1() {
+        String demo1 = null;
+        assertTrue(Utils.isBlank(demo1));
+    }
+
+    @Test
+    public void testIsBlank2() {
+        String demo2 = "";
+        assertTrue(Utils.isBlank(demo2));
+    }
+
+    @Test
+    public void testIsBlank3() {
+        String demo3 = "demo";
+        assertFalse(Utils.isBlank(demo3));
+    }
+
+    @Test
+    public void testBuildInsertSqlNoWhereStatement() {
+        String criteria = "student";
+        Set<String> columnNames = new HashSet<String>();
+        columnNames.add("name");
+        columnNames.add("age");
+
+        assertEquals("INSERT INTO student(age, name) VALUES (:age, :name)", Utils.buildInsertSql(columnNames, criteria).toString().trim());
+    }
+
+    @Test
+    public void testBuildInsertSqlWithWhereStatement() {
+        String criteria = "student where name = 'lishunli'";
+        Set<String> columnNames = new HashSet<String>();
+        columnNames.add("name");
+        columnNames.add("age");
+
+        assertEquals("INSERT INTO student (age, name) VALUES (:age, :name)", Utils.buildInsertSql(columnNames, criteria).toString().trim());
+    }
+
+    @Test
+    public void testParseString2Boolean1() {
+        assertFalse(Utils.parseString2Boolean("false", Boolean.TRUE));
+    }
+
+    @Test
+    public void testParseString2Boolean2() {
+        assertTrue(Utils.parseString2Boolean("true", Boolean.TRUE));
+    }
+
+    @Test
+    public void testParseString2Boolean3() {
+        assertTrue(Utils.parseString2Boolean("others != true", Boolean.TRUE));
+    }
+
+    @Test
+    public void testParseString2Boolean4() {
+        assertTrue(Utils.parseString2Boolean(null, Boolean.TRUE));
+    }
+
+    @Test
+    public void testParseString2Long1() {
+        assertEquals(5000, Utils.parseString2Long("5000", 0L).longValue());
+    }
+
+    @Test
+    public void testParseString2Long2() {
+        assertEquals(0, Utils.parseString2Long(null, 0L).longValue());
+    }
+
+    @Test
+    public void testParseString2Long3() {
+        assertEquals(0, Utils.parseString2Long("5000a", 0L).longValue());
+    }
+
+    @Test
+    public void testLoadPropertiesFileByStreamNormal() {
+        Properties prop = Utils.loadPropertiesFile(this.getClass().getClassLoader().getResourceAsStream("build-info.properties"));
+
+        assertNotNull(prop);
+        assertEquals("0.1-SANPSHOT", prop.get("version"));
+    }
+
+    @Test
+    public void testLoadPropertiesFileByStreamAbnormal() {
+        Properties prop = Utils.loadPropertiesFile(this.getClass().getClassLoader().getResourceAsStream("build-infoXXX.properties"));
+
+        assertNotNull(prop);
+        assertNull(prop.get("version"));
+    }
+
+
+    @Test
+    public void testLoadPropertiesFileByPathAndStreamNormal() {
+        Properties config = Utils.loadPropertiesFile(
+                System.getProperty("user.dir") + "\\" + "config.properties",
+                this.getClass().getClassLoader().getResourceAsStream("config.properties"));
+
+        assertNotNull(config);
+        assertEquals("false", config.get("openLogFile"));
+    }
+
+    @Test
+    public void testLoadPropertiesFileByPathAndStreamAbnormal() {
+        Properties config = Utils.loadPropertiesFile(
+                System.getProperty("user.dir") + "\\" + "config.properties",
+                this.getClass().getClassLoader().getResourceAsStream("configXXX.properties"));
+
+        assertNotNull(config);
+        assertNull(config.get("openLogFile"));
+    }
+}
