815080517dab75402e2dcabb584311f6292ca80c
lishunli.me@gmail.com <lishunli.me@gmail.com@fa5e0b39-550b-3b35-3821-e6856afb3cb5>
2012-01-07 07:08:35 +0000

    BUG: fix something wrong on open log when abnormal coping.
    git-svn-id: https://table-data-copier.googlecode.com/svn/trunk@8 fa5e0b39-550b-3b35-3821-e6856afb3cb5

--- src/main/java/com/googlecode/usc/OpenLogTask.java
+++ src/main/java/com/googlecode/usc/OpenLogTask.java
@@ -1,5 +1,6 @@
 package com.googlecode.usc;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -7,10 +8,22 @@ import java.io.IOException;
  * @author ShunLi
  */
 class OpenLogTask extends java.util.TimerTask {
+    private final static String NOTEPAD_PLUS_PLUS_PATH = "C:/Program Files/Notepad++/notepad++.exe";
+    private final static String NOTEPAD_PATH = "notepad.exe";
+
     @Override
     public void run() {
         try {
-            Runtime.getRuntime().exec("notepad.exe " + System.getProperty("user.dir") + "\\log\\copier.log");
+            String execName = null;
+
+            File file = new File(NOTEPAD_PLUS_PLUS_PATH);
+            if (file.exists()) {
+                execName = NOTEPAD_PLUS_PLUS_PATH;
+            } else {
+                execName = NOTEPAD_PATH;
+            }
+
+            Runtime.getRuntime().exec(execName + " " + System.getProperty("user.dir") + "\\log\\copier.log");
         } catch (IOException e) {
         }
     }
